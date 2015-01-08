9432ab9e484076ceb78adbd78126f153b5cc7d2c
ezabelin <egor.zabelin@softheme.com>
2015-01-08 13:24:11 +0200

    -Sixth home work is done

---
+++ HM6/DelegatesExercise1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DelegatesExercise1
+{
+    class Program
+    {
+        public delegate double Cualculator(int x, int y);
+
+        static void Main()
+        {
+            Cualculator add = (x, y) => x + y;
+            Cualculator sub = (x, y) => x - y;
+            Cualculator mul = (x, y) => x * y;
+            Cualculator div = (x, y) =>
+            {
+                if (y != 0)
+                {
+                    return (double) x/y;
+                }
+                return -1;
+            };
+
+            Console.WriteLine("Enter first number:");
+            int firstValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int secondValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter sign of operation:");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine("Result of sum is {0}", add(firstValue, secondValue));
+                    break;
+                case "-":
+                    Console.WriteLine("Result of subtraction is {0}", sub(firstValue, secondValue));
+                    break;
+                case "*":
+                    Console.WriteLine("Result of multiplication is {0}", mul(firstValue, secondValue));
+                    break;
+                case "/":
+                    double result = div(firstValue, secondValue);
+                    if (result > 0)
+                    {
+                        Console.WriteLine("Result of division is {0}", result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Impossible to delete on 0");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Entered operation is not supported");
+                    break;
+            }
+
+            Console.ReadKey();     
+        }
+    }
+}
---
+++ HM6/DelegatesExercise2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DelegatesExercise2
+{
+    class Program
+    {
+        private delegate int Randomizer();
+        private delegate double Average(Randomizer[] randomizers);
+
+        static void Main()
+        {
+            Randomizer randomizer1 = MakeRandomValue;
+            Randomizer randomizer2 = MakeRandomValue;
+            Randomizer randomizer3 = MakeRandomValue;
+            Randomizer randomizer4 = MakeRandomValue;
+            Randomizer randomizer5 = MakeRandomValue;
+
+            Randomizer[] randomizers =
+            {
+                randomizer1,
+                randomizer2,
+                randomizer3,
+                randomizer4,
+                randomizer5
+            };
+
+            Average avarageValue = delegate(Randomizer[] randomizersArray)
+            {
+                int sum = 0;
+
+                foreach (var element in randomizersArray)
+                {
+                    sum += element();
+                }
+
+                double avarage = (double)sum / randomizersArray.Length;
+                return avarage;
+            };
+
+            Console.WriteLine("Avarage value is {0}", avarageValue(randomizers));          
+            Console.ReadKey();
+        }
+
+        private static int MakeRandomValue()
+        {
+            Random ran = new Random();
+            int value = ran.Next(100);
+            return value;
+        }
+    }
+}
