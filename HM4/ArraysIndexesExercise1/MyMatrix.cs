4ecddcc615e176ab255e8253dd81538538bd1506
ezabelin <egor.zabelin@softheme.com>
2015-01-25 22:25:42 +0200

    -Added ability to add/delete row and columns for matrix in ArraysIndexesExercise1

--- HM4/ArraysIndexesExercise1/MyMatrix.cs
+++ HM4/ArraysIndexesExercise1/MyMatrix.cs
@@ -1,6 +1,8 @@
-﻿namespace ArraysIndexesExercise1
+﻿using System;
+
+namespace ArraysIndexesExercise1
 {
-    class MyMatrix
+    internal class MyMatrix
     {
         private int[,] matrix;
         public int Rows { get; set; }
@@ -17,6 +19,69 @@
         {
             get { return matrix[index1, index2]; }
             set { matrix[index1, index2] = value; }
+        }
+
+        public void AddRow()
+        {
+            Rows = Rows + 1;
+            CopyMatrix(Rows - 1, Columns);
+        }
+
+        public void DeleteRow()
+        {
+            if (Rows != 1)
+            {
+                Rows = Rows - 1;
+                CopyMatrix(Rows, Columns);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, impossible to delete single row of matrix"); 
+            }
+        }
+
+        public void AddColumn()
+        {
+            if (Columns != 1)
+            {
+                Columns = Columns + 1;
+                CopyMatrix(Rows, Columns - 1);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, impossible to delete single column of matrix");
+            }
+        }
+
+        public void DeleteColumn()
+        {
+            Columns = Columns - 1;
+            CopyMatrix(Rows, Columns);
+        }
+
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Console.Write("{0}\t", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void CopyMatrix(int rowsNumber, int columnsNumber)
+        {
+            var newMatrix = new int[Rows, Columns];
+            for (int i = 0; i < rowsNumber; i++)
+            {
+                for (int j = 0; j < columnsNumber; j++)
+                {
+                    newMatrix[i, j] = matrix[i, j];
+                }
+            }
+            matrix = newMatrix;
         }
     }
 }
--- HM4/ArraysIndexesExercise1/Program.cs
+++ HM4/ArraysIndexesExercise1/Program.cs
@@ -7,7 +7,7 @@ namespace ArraysIndexesExercise1
         static void Main()
         {
             Random random = new Random();
-            MyMatrix matrix = new MyMatrix(5, 4);
+            MyMatrix matrix = new MyMatrix(2, 4);
 
             Console.WriteLine("Matrix is: \n");
 
@@ -16,10 +16,42 @@ namespace ArraysIndexesExercise1
                 for (int j = 0; j < matrix.Columns; j++)
                 {
                     matrix[i, j] = random.Next(1, 100);
-                    Console.Write("{0}\t",matrix[i, j]);
                 }
-                Console.WriteLine();
             }
+
+            matrix.PrintMatrix();
+
+            matrix.AddColumn();
+            for (int i = 0; i < matrix.Rows; i++)
+            {
+                matrix[i, matrix.Columns - 1] = random.Next(1, 100);
+            }
+
+            Console.WriteLine("\nNew matrix with 1 added column is: \n");
+            matrix.PrintMatrix();
+
+            matrix.AddRow();
+            for (int i = 0; i < matrix.Columns; i++)
+            {
+                matrix[matrix.Rows - 1, i] = random.Next(1, 100);
+            }
+
+            Console.WriteLine("\nNew matrix with 1 added row is: \n");
+            matrix.PrintMatrix();
+
+            matrix.DeleteRow();
+            Console.WriteLine("\nNew matrix with 1 deleted row is: \n");
+            matrix.PrintMatrix();
+
+            matrix.DeleteColumn();
+            Console.WriteLine("\nNew matrix with 1 deleted row is: \n");
+            matrix.PrintMatrix();
+
+            Console.WriteLine("\nTrying to delete all rows in matrix");
+            matrix.DeleteRow();
+            Console.WriteLine("1 row has been deleted, matrix is:");
+            matrix.PrintMatrix();
+            matrix.DeleteRow();
 
             Console.ReadKey();
         }
