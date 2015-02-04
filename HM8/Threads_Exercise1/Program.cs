fc122fef555f685024121cb19f08a1bb22b24de8
ezabelin <egor.zabelin@softheme.com>
2015-02-04 15:33:04 +0200

    -Eighth home work (Threads) is done

---
+++ HM8/Threads_Exercise1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads_Exercise1
+{
+    class Program
+    {
+        private const int MaxX = 100;
+        private const int MaxY = 35;
+        private static Random _random;
+
+        static void Main()
+        {
+            Thread[] threadArray = new Thread[45];
+            for (int i = 0; i < threadArray.Length; i++)
+            {
+                //Console configuration
+                Console.SetWindowSize(MaxX+1, MaxY+1);
+                Console.WindowLeft = Console.WindowTop = 0;
+                Console.SetBufferSize(MaxX + 1, MaxY + 1);
+                Console.CursorVisible = false;
+
+                //Threads creating
+                ParameterizedThreadStart paramThread = new ParameterizedThreadStart(WriteLetter);
+                threadArray[i] = new Thread(paramThread);
+
+                //Set random length of strips
+                _random = new Random();
+                threadArray[i].Start(_random.Next(10, 20));
+                Thread.Sleep(200);
+            }
+        }
+
+        private static void WriteLetter(object counter)
+        {
+            int lineSizeCounter = 0;
+            _random = new Random();
+            int cursorX = _random.Next(0, MaxX-1);
+            int cursorY = _random.Next(0, MaxY-1);
+            while (lineSizeCounter < (int)counter)
+            {
+                char[] chars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+                int i = _random.Next(chars.Length);
+                if (lineSizeCounter == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (lineSizeCounter == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                }
+
+                GetValidVerticalCursorPostition(ref cursorY);
+                Console.SetCursorPosition(cursorX, cursorY);
+                Console.WriteLine(chars[i]);
+                Thread.Sleep(100);
+                lineSizeCounter++;
+            }
+            Console.ResetColor();
+        }
+
+        private static void GetValidVerticalCursorPostition(ref int cursorRandomY)
+        {
+            if (cursorRandomY < MaxY-1)
+            {
+                cursorRandomY = cursorRandomY + 1;
+            }
+            else if (cursorRandomY >= MaxY-1)
+            {
+                cursorRandomY = 0;
+            }
+        }
+    }
+}
---
+++ HM8/Threads_Exercise2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads_Exercise2
+{
+    class Program
+    {
+        private const int MaxX = 100;
+        private const int MaxY = 35;
+        private static Random Random = new Random();
+
+        static void Main()
+        {
+            Thread[] threadArray = new Thread[30];
+            for (int i = 0; i < threadArray.Length; i++)
+            {
+                //Console configuration
+                Console.SetWindowSize(MaxX + 1, MaxY + 1);
+                Console.WindowLeft = Console.WindowTop = 0;
+                Console.SetBufferSize(MaxX + 1, MaxY + 1);
+                Console.CursorVisible = false;
+
+                //Threads creating
+                var sameCoordinateX = Random.Next(0, MaxX - 1);
+                int stripsLength = Random.Next(10, 20);
+                threadArray[i] = new Thread(() => WriteLetter(stripsLength, sameCoordinateX));
+                threadArray[i].Start();
+                Thread.Sleep(300);
+            }
+        }
+
+        private static void WriteLetter(int counter, int coordX)
+        {
+            int lineSizeCounter = 0;
+            int cursorX = coordX;
+            int cursorY = Random.Next(0, (MaxY - 1) / 2);
+            int cursorY2 = Random.Next((MaxY - 1) / 2, MaxY - 1);
+            while (lineSizeCounter < counter)
+            {
+                char[] chars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+                int i = Random.Next(chars.Length);
+                if (lineSizeCounter == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (lineSizeCounter == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                }
+
+                GetValidVerticalCursorPostition(ref cursorY);
+                GetValidVerticalCursorPostition(ref cursorY2);
+
+                Console.SetCursorPosition(cursorX, cursorY);
+                Console.WriteLine(chars[i]);
+                Thread.Sleep(100);
+
+                Console.SetCursorPosition(cursorX, cursorY2);
+                Console.WriteLine(chars[i]);
+                Thread.Sleep(100);
+
+                lineSizeCounter++;
+            }
+            Console.ResetColor();
+        }
+
+        private static void GetValidVerticalCursorPostition(ref int cursorRandomY)
+        {
+            if (cursorRandomY < MaxY - 1)
+            {
+                cursorRandomY = cursorRandomY + 1;
+            }
+            else if (cursorRandomY >= MaxY - 1)
+            {
+                cursorRandomY = 0;
+            }
+        }
+    }
+}
